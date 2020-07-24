363650d124331d44828a5a257fe9019a957fb6a3
omershafique0 <omershafique0@gmail.com>
2020-07-24 16:48:24 +0500

    Final Commit

--- models/auth.models.js
+++ models/auth.models.js
@@ -1,41 +1,41 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+    const mongoose = require("mongoose");
+    const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
+    const UserSchema = new mongoose.Schema({
+        firstName: {
+            type: String,
+            required: true
+        },
 
-    lastName: {
-        type: String,
-        required: true
-    },
+        lastName: {
+            type: String,
+            required: true
+        },
 
-    email: {
-        type: String,
-        required: true
-    },
+        email: {
+            type: String,
+            required: true
+        },
 
-    password: {
-        type: String,
-        required: true
-    }
-})
+        password: {
+            type: String,
+            required: true
+        }
+    })
 
-UserSchema.pre('save', async function (next) {
-    try{
-        console.log("Password is : ", this.password);
-        const salt = await bcrypt.genSalt(10);
-        console.log("Salt is : ", salt);
-        const hash = await bcrypt.hash(this.password, salt);
-        console.log("Hash is : ", hash);
-        this.password = hash
-        next();
-    }
-    catch{
-        console.log("Error in hash", error)
-    }
-})
+    UserSchema.pre('save', async function (next) {
+        try{
+            console.log("Password is : ", this.password);
+            const salt = await bcrypt.genSalt(10);
+            console.log("Salt is : ", salt);
+            const hash = await bcrypt.hash(this.password, salt);
+            console.log("Hash is : ", hash);
+            this.password = hash
+            next();
+        }
+        catch{
+            console.log("Error in hash", error)
+        }
+    })
 
-module.exports = User = mongoose.model("User", UserSchema);
+    module.exports = User = mongoose.model("User", UserSchema);
